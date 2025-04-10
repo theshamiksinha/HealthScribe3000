dab70bb34d6918b31f5cda5b4617b25b027c32c2
shamiksinha <shamiksinha2003@outlook.com>
2025-04-10 16:24:43 +0530

    Adding Final

--- multi_perspective_health/utils/metrics.py
+++ multi_perspective_health/utils/metrics.py
@@ -19,7 +19,7 @@ def compute_token_f1(preds, labels, label_map):
     # preds = [[label_map[i] for i in seq] for seq in preds]
     labels = [[label_map[i] for i in seq] for seq in labels]
     preds = [[label_map[i] for i in seq if i != 0] for seq in preds]
-    # labels = [[label_map[i] for i in seq if i != 0] for seq in labels]
+    labels = [[label_map[i] for i in seq if i != 0] for seq in labels]
 
     print(len(preds))
     print(len(labels))
