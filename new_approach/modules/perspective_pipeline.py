ee7abb057de29512e1330fa904b5968749c75181
shamiksinha <shamiksinha2003@outlook.com>
2025-04-13 22:25:23 +0530

    Adding Final

--- new_approach/modules/perspective_pipeline.py
+++ new_approach/modules/perspective_pipeline.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from training.train_classifier import train_classifier, evaluate
 from models.perspective_classifier import PerspectiveClassifier
 from transformers import AutoTokenizer
